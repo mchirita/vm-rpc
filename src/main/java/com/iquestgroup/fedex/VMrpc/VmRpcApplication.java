f52796e79851e181323c479b8ee62c23c65f1a09
Mihai Chirita <mihai.chirita@iquestgroup.com>
2018-11-16 10:24:32 +0200

    Initial project setup

---
+++ src/main/java/com/iquestgroup/fedex/VMrpc/VmRpcApplication.java
@@ -0,0 +1,12 @@
+package com.iquestgroup.fedex.VMrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmRpcApplication.class, args);
+	}
+}
